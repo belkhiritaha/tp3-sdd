15c9f2e997365cf13a2ac12a1affa7c767d6e717
Taha Belkhiri <belkhiri.taha53@gmail.com>
2022-05-03 09:22:04 +0200

    question 1 working

--- main.c
+++ main.c
@@ -17,6 +17,12 @@ void printTree(Tree_t *tree) {
     printTree(tree->right);
 }
 
+void printlistree(Tree_t ** listree, int listree_size){
+    for (int i = 0; i < listree_size; i++) {
+        printf(" %p : %d left : %p , right : %p \n",listree[i], listree[i]->value, listree[i]->left, listree[i]->right);
+    }
+}
+
 
 int main(){
     Tree_t * root = NULL;
@@ -26,41 +32,66 @@ int main(){
         return 1;
     }
 
+    Tree_t ** listree = malloc(sizeof(Tree_t *) * 100);
+    int listree_size = 0;
+
     // while file not ended read values
     int value;
-    while (fscanf(fp, "%d", &value) != EOF) {
+    while (fscanf(fp, "%d", &value) != EOF)
+    {
         Tree_t * new_node = (Tree_t *) malloc(sizeof(Tree_t));
         new_node->value = value;
         new_node->left = NULL;
         new_node->right = NULL;
 
         // if root is empty
-        if (root == NULL) {
+        if (root == NULL) 
+        {
             root = new_node;
-        } else {
+            //append to listree
+            listree[listree_size] = new_node;
+            listree_size++;
+        } 
+        else 
+        {
             Tree_t * current = root;
-            while (current != NULL) {
-                if (current->value > value) {
-                    if (current->left == NULL) {
+            while (current != NULL) 
+            {
+                if (current->value > value)
+                {
+                    if (current->left == NULL) 
+                    {
                         current->left = new_node;
                         break;
-                    } else {
+                    }
+
+                    else 
+                    {
                         current = current->left;
                     }
-                } else {
-                    if (current->right == NULL) {
+                } 
+
+                else 
+                {
+                    if (current->right == NULL) 
+                    {
                         current->right = new_node;
                         break;
-                    } else {
+                    }
+
+                    else 
+                    {
                         current = current->right;
                     }
                 }
             }
+            //append to listree
+            listree[listree_size] = new_node;
+            listree_size++;
         }
     }
 
-    printTree(root);
-
+    printlistree(listree, listree_size);
 
 
 }
